fe369158ebc76f51eda7c18d8a66e9f0ce54d8a6
DotRandomDot <57591549+DotRandomDot@users.noreply.github.com>
2026-04-02 19:21:17 +0200

    removed api-key.txt, added .env file which should contain api keys for gemini and newsapi site(currently only for newsapi), added program.py, which contains code for fetching news and a function for creating a web API endpoint

---
+++ program.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify
+import requests
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+
+app = Flask(__name__)
+
+genai.configure(api_key = os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-pro")
+
+# Code For Fetching News
+def fetch_news():
+    url = "https://newsapi.org/v2/top-headlines"
+    params = {
+        "category": "general",
+        "pageSize": 10,
+        "apiKey": os.getenv("NEWS_API_KEY")
+    }
+
+    response = requests.get(url, params = params)
+    data = response.json()
+    return data["articles"]
+
+# API Endpoint
+@app.route("/news-summary")
+def news_summary():
+    try:
+        articles = fetch_news()
+
+        return jsonify({
+            "articles": [
+                {"title": a["title"], "url": a["url"]}
+                for a in articles
+            ]
+        })
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+if __name__ == "__main__":
+    app.run(debug = True)
